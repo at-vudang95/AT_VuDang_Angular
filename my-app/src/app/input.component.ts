f6deb0eb1dbb9a1e3910e708e8ee1563e86f0cd2
at-vudang <ngocvupct1995@gmail.com>
2017-07-26 14:57:22 +0700

    Update v3.0

--- my-app/src/app/input.component.ts
+++ my-app/src/app/input.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output, OnInit, ViewChild} from '@angular/core';
 import { Member } from './member';
 import { MyService } from './service';
+import {AppComponent} from './app.component';
 @Component({
-  selector: 'my-input',
+  selector: 'app-input',
   templateUrl: './input.component.html'
 })
-export class InputComponent {
-  name:string;
-  constructor(private _sharedService: MyService){
-     this.name = 'hello';
+export class InputComponent implements OnInit {
+  name: string;
+  @Output() result: EventEmitter<any> = new EventEmitter<any>();
+  constructor(private _sharedService: MyService) {
+    this.name = 'hello';
   }
-  newMember(){
-    //let member:Member = new Member(this.name);
-  //  console.log(this.name);
-  //  console.log(member.$domElm);
-    this._sharedService.insertData(this.name);
-    this.name = '';
-  } 
+
+  ngOnInit() {
+
+  }
+  newMember() {
+    // this.app2.member = this.name;
+
+    // let member:Member = new Member(this.name);
+    //  console.log(this.name);
+    //  console.log(member.$domElm);
+    // this._sharedService.insertData(this.name);
+    // this.name = '';
+     this.result.emit(this.name);
+  }
 }
--- my-app/src/app/list.component.ts
+++ my-app/src/app/list.component.ts
@@ -1,20 +1,25 @@
-import { Component,OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 // import { Member } from './member';
 import { MyService } from './service';
 @Component({
-  selector: 'my-list',
+  selector: 'app-list',
   templateUrl: './list.component.html'
 })
 export class ListComponent {
-  data: string[] = [];
-  constructor(private _sharedService: MyService){
-            
+  // data: string[] = [];
+  @Input() data: any;
+  @Output() result2: EventEmitter<any> = new EventEmitter<any>();
+  constructor(private _sharedService: MyService) {
+    console.log('constructor');
   }
-  ngOnInit():any {
-    this.data = this._sharedService.dataArray;
+  OnInit(): any {
+    console.log('ngOnInit');
+    // this.data = this._sharedService.dataArray;
+    this.data = [];
   }
-  deleteMember(i:number){
-      //this.data = this.data.splice(i+1,1);
-      this._sharedService.deleteData(i);
+  deleteMember(i: number)  {
+      // this.data = this.data.splice(i+1,1);
+      // this._sharedService.deleteData(i);
+      this.result2.emit(i);
   }
 }
