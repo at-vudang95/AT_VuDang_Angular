c28eb55561ee9cf3090cf7bd14c062706f9bd8b8
at-vudang <ngocvupct1995@gmail.com>
2017-07-29 17:27:18 +0700

    Exercise directive angular

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BirthdayDirective } from './birthday.directive';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, BirthdayDirective
   ],
   imports: [
     BrowserModule
